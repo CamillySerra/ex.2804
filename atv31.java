ef06aec760c0464f8638dc422731ab97afccc231
CamillySerra <972512367@ulife.com.br>
2025-04-28 14:15:57 -0300

    primeiro commit

---
+++ atv31.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class atv31 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numeros = new int[3];
+
+        System.out.print("Digite o primeiro número: ");
+        numeros[0] = sc.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        numeros[1] = sc.nextInt();
+
+        System.out.print("Digite o terceiro número: ");
+        numeros[2] = sc.nextInt();
+
+        Arrays.sort(numeros);
+
+        System.out.println("Números em ordem crescente:");
+        for (int num : numeros) {
+            System.out.println(num);
+        }
+
+        sc.close();
+    }
+}
